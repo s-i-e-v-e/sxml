7e5836282e146e8c1b23baee61274c2e76fe9a35
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2024-04-25 21:31:20 +0000

    update dependencies

--- src/lex.ts
+++ src/lex.ts
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import {CharacterStream, TokenStream} from "@sieve/nonstd";
+import {CharacterStream, TokenStream} from "@sieve/nonstd/data";
 
 const OpenBracket = new Set(["[", "{", "(", ]);
 const CloseBracket = new Set(["]", "}", ")", ]);
